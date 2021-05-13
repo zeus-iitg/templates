6905ed5537f2e0f3c6b02151604e2ea66ba8c153
zeus-iitg <vaibhavsingh878@gmail.com>
2021-05-14 01:49:39 +0530

    initial commit

---
+++ test case creator/for_input.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/__gcd((a),(b))
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vi>
+#define mt make_tuple
+#define mp make_pair
+#define hell 1000000007
+#define mod 998244353
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define all(a) a.begin(),a.end()
+#define ign cin.ignore();
+#define g(a,b) get<a>(b)
+#define vt(a) vector<a>
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repi(i,a,b) for(ll i=b-1;i>=a;i--)
+#define fi first
+#define se second
+
+#define o_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define o_setll tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+//to delete an element
+//s.erase(s.find_by_order(s.order_of_key(ele)));
+//where s is the name of the ordered_set
+//member functions :
+//1. order_of_key(k) : number of elements strictly lesser than k
+//2. find_by_order(k) : k-th element in the set
+
+using namespace std;
+using namespace __gnu_pbds;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+ofstream fout;
+
+int main()
+{
+    IOS
+    /*
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+    auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
+    auto val_rand = std::bind(std::uniform_int_distribution<long long>(0,1000000000000000000),
+                           mt19937(seed));
+    //creates 10 files
+    ll ntc=10;
+    rep(tc,0,ntc)
+    {
+        //cout<<"Case #"<<tc<<": ";
+        string filename = "input0" + to_string(tc) + ".txt";
+        fout.open(filename);
+
+        fout.close();
+    }
+    return 0;
+}
